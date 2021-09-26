2df7925882e06610430b2012dd3a6c01d5287484
Michael Ivankovich <91423379+esoftcons@users.noreply.github.com>
2021-09-26 19:22:53 +0300

    Update Program.java

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -4,6 +4,7 @@ public class Program {
     public static void main(String[] args) {
         Numbers numbers = new Numbers();
         int r=numbers.sum(5);
+               int r=numbers.sum(5);
         System.out.println(r);
     }
 }
